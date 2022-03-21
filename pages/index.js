547e4ecfde4e51c60d1347869e0e0162f5992861
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-03-21 22:54:52 +0800

    ✨ add first call to get ProductCollection

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,43 @@
 import Head from "next/head";
+import { GraphQLClient, gql } from "graphql-request";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const endPoint =
+    "https://graphql.contentful.com/content/v1/spaces/0s0kjd52m3s3";
+
+  const graphQLClient = new GraphQLClient(endPoint, {
+    headers: {
+      authorization: `Bearer TpBId6BcrKUm2Ej8S_-xohiBoJaRkQAbTDM5Lll41po`,
+    },
+  });
+
+  const productCollections = gql`
+    {
+      produkCollection(order: hargaProduk_ASC) {
+        items {
+          namaProduk
+          hargaProduk
+          fotoProdukCollection {
+            items {
+              url
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const products = await graphQLClient.request(productCollections);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+export default function Home({ products }) {
+  console.log(products);
   return (
     <div>
       <Head>
