b1c95087c849a7a6b66209e0c6b647b57284d3d0
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-03-23 00:03:19 +0800

    ✨ update css

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '../lib/apolloClient';
-import '../styles/reset.css';
+
 import '../styles/globals.css';
-import '../styles/carousels.css';
-import '../styles/tentangKami.css';
-import '../styles/ProdukJualan.css';
 
 function MyApp({ Component, pageProps }) {
   return (
