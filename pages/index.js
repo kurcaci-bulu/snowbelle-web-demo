de5383e91f59904dcad41d03ca92c418703fc6af
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-03-23 00:19:46 +0800

    ✨ enable ssr

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { useQuery, gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 import Carousel from '../components/Carousel';
 import TentangKami from '../components/TentangKami';
 import ProdukJualan from '../components/ProdukJualan';
+import apolloClient from '../lib/apolloClient';
 const productCollections = gql`
   query getProductCollections {
     produkCollection(order: hargaProduk_ASC) {
@@ -19,8 +20,18 @@ const productCollections = gql`
   }
 `;
 
-export default function Home() {
-  const { data } = useQuery(productCollections);
+export const getServerSideProps = async () => {
+  const { data } = await apolloClient.query({
+    query: productCollections,
+  });
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ data }) {
   return (
     <div>
       <Head>
