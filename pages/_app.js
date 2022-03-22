d0064f2635fcd9525d62091fd6903d174b5d5829
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-03-22 20:52:30 +0800

    ✨ add apolloClient and provide it to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-import "../styles/reset.css";
-import "../styles/globals.css";
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../lib/apolloClient';
+import '../styles/reset.css';
+import '../styles/globals.css';
+import '../styles/carousels.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
