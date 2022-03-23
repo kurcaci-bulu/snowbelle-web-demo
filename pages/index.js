68b00858c6087b1e3e1fa8c35b64916ff09f9695
Cipto Hartanto <cipto.hartanto@yahoo.com>
2022-03-23 10:41:02 +0800

    🔥 remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import Head from 'next/head';
-import { gql } from '@apollo/client';
+
 import Carousel from '../components/Carousel';
 import TentangKami from '../components/TentangKami';
 import ProdukJualan from '../components/ProdukJualan';
 import apolloClient from '../lib/apolloClient';
-const productCollections = gql`
-  query getProductCollections {
-    produkCollection(order: hargaProduk_ASC) {
-      items {
-        namaProduk
-        hargaProduk
-        fotoProdukCollection {
-          items {
-            url
-          }
-        }
-      }
-    }
-  }
-`;
-
-export const getStaticProps = async () => {
-  const { data } = await apolloClient.query({
-    query: productCollections,
-  });
-  return {
-    props: {
-      data,
-    },
-  };
-};
 
 export default function Home({ data }) {
   return (
